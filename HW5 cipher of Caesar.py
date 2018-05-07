12101c55b9a50e08f0dea0ba6b234f9edeaab4bc
Andrey Kopylkov <andreykopylkov@gmail.com>
2018-05-07 18:31:49 +0300

    Initial commit

--- HW5 cipher of Caesar.py
+++ HW5 cipher of Caesar.py
@@ -1,13 +1,14 @@
 message = input('Введите текст на русском языке для зашифровки: ').lower()
 alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
       'п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
+shift = int(input('Введите размер смещения: '))
 new_mes = []
 new_mes2 = ''
 for i in message:
     if i not in alph:
              new_mes.append(i)
     else:
-        new_mes.append(alph[alph.index(i) + 3])
+        new_mes.append(alph[alph.index(i) + shift])
 #with open('word.txt','w') as f:
 #    for i in new_mes:
 #        f.write(i)
