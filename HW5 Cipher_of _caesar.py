a6d6cfc4fe920cabc7ebf6178016f744dcad78c9
Andrey Kopylkov <andreykopylkov@gmail.com>
2018-05-16 16:23:19 +0300

    Initial commit

--- HW5 Cipher_of _caesar.py
+++ HW5 Cipher_of _caesar.py
@@ -2,13 +2,13 @@ message = input('Введите текст на русском языке для
 alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
       'п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
 shift = int(input('Введите размер смещения: '))
-new_mes = []
-new_mes2 = ''
+new_mes_range = []
+new_mes = ''
 for i in message:
     if i not in alph:
-             new_mes.append(i)
+             new_mes_range.append(i)
     else:
-        new_mes.append(alph[alph.index(i) + shift])
-for i in range(len(new_mes)):
-    new_mes2 += new_mes[i]
-print('Зашифрованное сообщение: ', new_mes2)
+        new_mes_range.append(alph[alph.index(i) + shift])
+for i in range(len(new_mes_range)):
+    new_mes += new_mes_range[i]
+print('Зашифрованное сообщение: ', new_mes)
