3b0400fc326358f5667f64c54c2fd5d14d20e3ed
Andrey Kopylkov <andreykopylkov@gmail.com>
2018-05-21 18:03:18 +0300

    Initial commit

--- HW6 Cipher of Vigener.py
+++ HW6 Cipher of Vigener.py
@@ -1,15 +1,14 @@
 message = input('Введите текст на русском языке для зашифровки: ').lower()
-key = input('Введите слово-ключ')
+key = input('Введите слово-ключ: ')
 alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
       'п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
-new_mes_range = []
-new_mes = ''
+newMesRange = []
+newMes = ''
 for i in message:
     if i not in alph:
-             new_mes_range.append(i)
+             newMesRange.append(i)
     else:
-        new_mes_range.append(alph[alph.index(i) + shift])
-for i in range(len(new_mes_range)):
-    new_mes += new_mes_range[i]
-print('Зашифрованное сообщение: ', new_mes)
-new_mes[i] = (message[i] + key[j]) % 33
+        newMesRange.append(alph[(alph.index(i) + len(key)) % len(alph)])
+for i in range(len(newMesRange)):
+    newMes += newMesRange[i]
+print('Зашифрованное сообщение: ', newMes)
