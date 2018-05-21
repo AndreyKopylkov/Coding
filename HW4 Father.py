159581a611372c8344c25edaf7f300bae12d137c
Andrey Kopylkov <andreykopylkov@gmail.com>
2018-05-21 17:37:55 +0300

    Initial commit

--- HW4 Father.py
+++ HW4 Father.py
@@ -1,7 +1,27 @@
-dict = {'Logo': 'Абельсон, Гарольд',
-        'APL': 'Айверсон, Кеннет',
-        'C++': 'Александреску, Андрей', 'Брайт, Уолтер',
-        'AWK': 'Ахо, Альфред',
-        'CoffeeScript': 'Ашкенас, Джереми',
-        'D': 'Брайт, Уолтер',
-        'Фортран': ''}
+#coding=utf-8
+fathers = {'Harold Abelson': 'Logo',
+        'Kenneth Eugene Iverson': 'APL',
+        'Andrei Alexandrescu': 'C++',
+        'Alfred Vaino Aho': 'AWK',
+        'Guido van Rossum': 'Python',
+        'Jeremy Ashkenas': 'CoffeeScript',
+        'Walter Bright': 'D',
+        'John George Kemeny': 'Basic',
+        'Peter Naur': 'Algol',
+        'Don Syme': 'F#'
+        }
+choiceAction = input('Что вы хотите сделать? Введите Sorted или Search: ')
+if choiceAction == 'Sorted':
+        sortName = sorted(fathers.keys())
+        for lang in sortName:
+                print(lang, " - ", fathers[lang])
+elif choiceAction == 'Search':
+        searchName = input("Кого вы хотите найти? Введите имя: ")
+        for name, value in fathers.items():
+                if name == searchName:
+                        print(searchName, 'участовал в разработке языка', fathers[name])
+                        break
+        else:
+                print("Не удалось найти этого человека")
+else:
+        print('Попробуйте ещё раз, возможно вы ввели команду неправильно')
---
+++ HW6 Cipher of Vigener.py
@@ -0,0 +1,15 @@
+message = input('Введите текст на русском языке для зашифровки: ').lower()
+key = input('Введите слово-ключ')
+alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+      'п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
+new_mes_range = []
+new_mes = ''
+for i in message:
+    if i not in alph:
+             new_mes_range.append(i)
+    else:
+        new_mes_range.append(alph[alph.index(i) + shift])
+for i in range(len(new_mes_range)):
+    new_mes += new_mes_range[i]
+print('Зашифрованное сообщение: ', new_mes)
+new_mes[i] = (message[i] + key[j]) % 33
